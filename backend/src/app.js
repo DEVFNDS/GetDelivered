164d5e72d0afff968b154d9c4dde4876a7853045
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2023-11-11 16:19:23 -0500

    readme and mongo connection changes

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+// app.js
+const connectDB = require('../src/db/db.js'); // Adjust the path accordingly
+
+// Connect to MongoDB
+connectDB();
+
+// Now you can continue with the rest of your application logic
+// ...
