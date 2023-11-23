e0a657ab396acc2227fa0b30e1c0172bb6e55be4
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2023-11-22 20:45:35 -0500

    user authentication backend addition

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,42 @@
 // app.js
 const connectDB = require('../src/db/db.js'); // Adjust the path accordingly
 
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000
+
+
+User = require('./models/userModel.js'),
+bodyParser = require('body-parser'),
+jsonwebtoken = require("jsonwebtoken");
+
 // Connect to MongoDB
 connectDB();
 
-// Now you can continue with the rest of your application logic
-// ...
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+var routes = require('./routes/routes.js');
+routes(app);
+
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
+
+app.listen(port);
+
+console.log(' RESTful API server started on: ' + port);
+
+module.exports = app;
