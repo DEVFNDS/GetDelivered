a56722cc777e70364089302280a95be77032acce
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2023-12-11 23:04:01 -0500

    file extension changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -44,7 +44,9 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null,  file.fieldname + '-' + uniqueSuffix);
+    const originalExtension = file.originalname.split('.').pop(); // Get the original file extension
+    const filename = file.fieldname + '-' + uniqueSuffix + '.' + originalExtension;
+    cb(null, filename);
   },
 });
 
