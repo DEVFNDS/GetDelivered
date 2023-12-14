182360b7a1b0ca447ba87916d4514070576a3a1a
Elwin Paulson <epaulvettiankal@gmail.com>
2023-12-13 20:44:45 -0500

    Footer added

--- frontend/grocery/src/App.js
+++ frontend/grocery/src/App.js
@@ -3,12 +3,13 @@ import './App.scss';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin';
 import Home from './components/Home';
+import Footer from './Footer';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Home /><Footer/></>} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
